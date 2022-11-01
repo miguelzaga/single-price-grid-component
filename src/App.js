1a7f180bb3067df4d9b31c91931913de838f6724
Miguel <miguezaga2@hotmail.com>
2022-11-01 18:20:14 -0500

    setup project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div>
-      <h1>New App</h1>
-    </div>
+    <section className="component">
+      <div>
+        <h1>Join our community</h1>
+        <h2>30-day, hassle-free money back guarantee</h2>
+        <p>
+          Gain access to our full library of tutorials along with expert code
+          reviews. Perfect for any developers who are serious about honing their
+          skills.
+        </p>
+      </div>
+
+      <div>
+        <h2> Monthly Subscription</h2>{" "}
+        <p>
+          <b>&dollar;29</b> per month
+        </p>
+        <p> Full access for less than &dollar;1 a day</p>
+        <button> Sign Up</button>
+      </div>
+      <div>
+        <h2>Why Us</h2>
+        <ul>
+          <li>Tutorials by industry experts</li>
+          <li>Peer &amp; expert code review</li>
+          <li>Coding exercises</li>
+          <li>Access to our GitHub repos</li>
+          <li>Community forum</li>
+          <li>Flashcard decks </li>
+          <li>New videos every week</li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
